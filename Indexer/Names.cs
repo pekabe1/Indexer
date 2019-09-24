8bc1fdc3f8068d29833fa63df2a0601ef8a41936
pekabe1 <kziarkowskiz@gmail.com>
2019-09-24 17:08:57 +0200

    Add project files.

---
+++ Indexer/Names.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    public class Names
+    {
+        List<string> namelist = new List<string>();
+        public void Add(string name)
+        {
+            namelist.Add(name);
+        }
+        public 
+
+    }
+}
---
+++ Indexer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Names name = new Names();
+            name.Add("Anna"); //1
+            name.Add("David"); //2
+            name.Add("Zenek"); //3
+            int i = name["Anna"]; // i = 2
+            Console.Read();
+        }
+    }
+}
